d124eb420b90255572d47a51f09f18dd90a39de0
antikantian <antikantian@users.noreply.github.com>
2016-04-29 14:44:07 -0400

    ...

--- src/main/scala/co/quine/gatekeeper/actors/ServerActor.scala
+++ src/main/scala/co/quine/gatekeeper/actors/ServerActor.scala
@@ -1,10 +1,11 @@
 package co.quine.gatekeeper.actors
 
 import akka.actor._
-import akka.event.LoggingReceive
 import akka.io.{IO, Tcp}
 
 import java.net.{InetSocketAddress, URI}
+
+import co.quine.gatekeeper.protocol._
 
 object ServerActor {
   def props(listen: URI): Props = Props(new ServerActor(listen))
@@ -16,8 +17,34 @@ class ServerActor(listen: URI) extends Actor with ActorLogging {
 
   override def preStart() = log.info("Tcp server: up")
 
-  def receive: Receive = LoggingReceive {
-    case _: Tcp.Connected => sender() ! Tcp.Register(context.actorOf(Props[ClientActor], "gatekeeper-client"))
+  def receive = {
+    case Tcp.Connected(remote, local) =>
+      val clientActor = context.actorOf(ConnectionActor.props(self))
+      sender ! Tcp.Register(clientActor)
+      log.info(s"${remote.getHostString} connected")
+    case Tcp.Bound(localAddress) =>
+      log.info(s"Server bound to $localAddress")
+    case Tcp.CommandFailed(cmd) =>
+      log.info(s"Command failed: $cmd")
+  }
+}
+
+object ConnectionActor {
+  def props(server: ActorRef): Props = Props(new ConnectionActor(server))
+}
+
+class ConnectionActor(server: ActorRef) extends Actor with ActorLogging {
+
+  def receive = {
+    case Tcp.Received(data) => println(data.utf8String)
+    case Command.Disconnect =>
+      log.info("Client disconnected")
+      context.stop(self)
+    case Tcp.PeerClosed =>
+      log.info("Client disconnected")
+      context.stop(self)
+    case msg => log.info(s"Received unknown: $msg")
   }
 
 }
+
---
+++ src/main/scala/co/quine/gatekeeper/protocol/Command.scala
@@ -0,0 +1,7 @@
+package co.quine.gatekeeper.protocol
+
+object Command {
+
+  case object Disconnect
+
+}
